40cedc19fb7b64dce35bb0933b680bf2fa5eba21
jfenuk <jfenuk@gmail.com>
2015-07-27 14:28:35 +0300

    First commit.

---
+++ src/main/java/com/fenuk/outlay/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.fenuk.outlay.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("main.do")
+	public String view (){
+		
+		return "main";
+	}
+	
+}
