c1618c76b8a0fe63ef94c40d61ec5eef1e71ffc6
jfenuk <jfenuk@gmail.com>
2015-08-04 16:30:24 +0300

    toString, hashCode and equals methods for Outlay class were implemented via commons.lang3

--- src/main/java/com/fenuk/outlay/model/Outlay.java
+++ src/main/java/com/fenuk/outlay/model/Outlay.java
@@ -1,12 +1,14 @@
 package com.fenuk.outlay.model;
 
-import java.math.BigDecimal;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class Outlay {
 
 	private int id;
 
-	private BigDecimal amount;
+	private long amount;
 
 	public int getId() {
 		return id;
@@ -16,45 +18,27 @@ public class Outlay {
 		this.id = id;
 	}
 
-	public BigDecimal getAmount() {
+	public long getAmount() {
 		return amount;
 	}
 
-	public void setAmount(BigDecimal amount) {
+	public void setAmount(long amount) {
 		this.amount = amount;
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Outlay))
-			return false;
-		Outlay other = (Outlay) obj;
-		if (id != other.id)
-			return false;
-		if (amount == null) {
-			if (other.amount != null)
-				return false;
-		} else if (!amount.equals(other.amount))
-			return false;
-		return true;
+		return EqualsBuilder.reflectionEquals(this, obj);
 	}
 
 	@Override
 	public String toString() {
-		return "Outlay [id=" + id + ", amount=" + amount + "]";
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
